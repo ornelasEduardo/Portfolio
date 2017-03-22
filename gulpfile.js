6ae558fc934ff13103505dd6d71edbbeaf3009e7
Eddie Ornelas <eddiie.ornelas@gmail.com>
2017-03-21 21:42:27 -0700

    Added a few things. Mostly just made some html and sass changes. Chipping at this project lol

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const uglifyCss = require('gulp-clean-css');
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
+const htmlmin = require('gulp-htmlmin');
 
 
 gulp.task('imagemin', () =>
@@ -16,7 +17,7 @@ gulp.task('imagemin', () =>
 );
 
 gulp.task('uglify-js', () =>
-  gulp.src('scripts/**/*.js')
+  gulp.src('scripts/*.js')
     .pipe(babel({
       presets: ['es2015']
     }))
@@ -27,7 +28,7 @@ gulp.task('uglify-js', () =>
 );
 
 gulp.task('uglify-css', ['sass-compile'], () =>
-  gulp.src('styles/**/*.css')
+  gulp.src('styles/*.css')
     .pipe(concat('main.css'))
     .pipe(uglifyCss())
     .pipe(rename({suffix: '.min'}))
@@ -35,15 +36,22 @@ gulp.task('uglify-css', ['sass-compile'], () =>
 );
 
 gulp.task('sass-compile', () =>
-  gulp.src('styles/sass/**/*.scss')
+  gulp.src('styles/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('styles/'))
 );
 
+gulp.task('html-min', () =>
+  gulp.src('index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/'))
+);
+
 gulp.task('watch', function(){
   gulp.watch('scripts/**/*.js', ['uglify-js']),
-  gulp.watch('styles/sass/**/*.scss', ['sass-compile', 'uglify-css']),
+  gulp.watch('styles/*.scss', ['sass-compile', 'uglify-css']),
   gulp.watch('images/**.*', ['imagemin'])
+  gulp.watch('index.html', ['html-min'])
 });
 
-gulp.task('default', ['imagemin', 'uglify-js', 'uglify-css', 'watch']);
+gulp.task('default', ['imagemin', 'uglify-js', 'uglify-css', 'html-min', 'watch']);
